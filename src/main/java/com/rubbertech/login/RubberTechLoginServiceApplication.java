31fdea46dec198cfe4b3439d4f7a0cb148a70a98
Zafar imam <zafarimam7032@gmail.com>
2022-08-28 11:36:59 +0530

    initial commit

---
+++ src/main/java/com/rubbertech/login/RubberTechLoginServiceApplication.java
@@ -0,0 +1,13 @@
+package com.rubbertech.login;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RubberTechLoginServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RubberTechLoginServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rubbertech/product/RubberTechLoginServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rubbertech.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RubberTechLoginServiceApplicationTests {
+
+//	@Test
+	void contextLoads() {
+	}
+
+}
